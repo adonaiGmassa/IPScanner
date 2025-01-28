47758c49a941573522756188a3a66dce81ed793a
Massa Adonaï <167198345+adonaiGmassa@users.noreply.github.com>
2025-01-28 21:52:19 +0100

    Création classe GestionReseau

---
+++ IpScannerApp/gestionReseau.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpScannerApp
+{
+    public class gestionReseau
+    {
+        public void GetIp (){ }
+        // Méthode pour déterminer la classe d'une adresse IP
+        public string ConnaitreClasse(string ipAddress)
+        {
+            
+        }
+
+        public void SetIp() { }
+    }
+
+}
+
